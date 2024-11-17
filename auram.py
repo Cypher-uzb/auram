bc60332a59450380cec3c0325f7decbbac2f2058
YourGitHubUsername <YourEmail@example.com>
2024-11-17 10:16:14 +0500

    auram

---
+++ auram.py
@@ -0,0 +1,18 @@
+bot_token="7647519119:AAFxvc6bu50UkWbMXW47Z0e4ubTXUblh780"
+
+from aiogram import Bot, Dispatcher, executor, types
+from config import bot_token
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(bot_token)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    first_name = message.from_user.first_name  
+    await message.answer(f"Salom {first_name}\n\n\nBot @cypher_uzb tomonidan yoqildi!")
+
+if __name__ == "__main__":  
+    executor.start_polling(dp, skip_updates=True)
